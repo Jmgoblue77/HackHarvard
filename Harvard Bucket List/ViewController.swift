ddc29f732d07ad3489e12f45fb516639e1d189f7
Jmgoblue77 <jason_ren@college.harvard.edu>
2016-10-22 14:50:16 -0400

    Initial Commit

---
+++ Harvard Bucket List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Harvard Bucket List
+//
+//  Created by Jason Ren on 10/22/16.
+//  Copyright © 2016 Jason Ren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
